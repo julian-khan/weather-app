064816bbf74cd1cff7b5fea2c04ac6c99f8e9ebc
Julian Khan <juliankhan.b@gmail.com>
2022-10-24 20:34:46 +0800

    Refactor App.js and LocationForm.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ function App() {
       {`Current weather: `}
     <form>
       <button>test</button>
-      {locationName}
     </form>
    
     </div>
--- src/LocationForm.js
+++ src/LocationForm.js
@@ -12,7 +12,6 @@ export default function LocationForm({changeLocationName}) {
     function handleSubmit(event) {
         event.preventDefault();
         changeLocationName(formValue);
-        //event.target.value ? setFormValue(event.target.value) : setFormValue('');
     };
 
     return (
@@ -22,7 +21,6 @@ export default function LocationForm({changeLocationName}) {
             <input type="text" value={formValue} onChange={handleChange}/>
             </label>
             <input type="submit" value="Submit" />
-          
         </form>
         );
     };
