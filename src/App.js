2f83b17120820b91680a29d37708473064956dcd
Julian Khan <juliankhan.b@gmail.com>
2022-10-24 20:33:42 +0800

    Create locationName variable that receives its value from form input

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import LocationForm from './LocationForm';
 function App() {
   const [weatherData, setWeatherData] = useState(null);
 
+  const [locationName, setLocationName] = useState('Perth!2!');
+
+  //delete below func?
+  const changeLocationName = (location) => {
+    setLocationName(location);
+  };
+
+
   useEffect(() => {
     fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=' + 
     'b1ab53cd687ca486f76e739d8fc1a32b')
@@ -19,10 +27,11 @@ function App() {
   return (
     <div>
       
-       <LocationForm />
+       <LocationForm changeLocationName={changeLocationName}/>
       {`Current weather: `}
     <form>
       <button>test</button>
+      {locationName}
     </form>
    
     </div>
--- src/LocationForm.js
+++ src/LocationForm.js
@@ -1,26 +1,25 @@
 import {useState, useEffect} from 'react';
 
-export default function LocationForm(props) {
-
-    const [location, setLocation] = useState('Perth');
+export default function LocationForm({changeLocationName}) {
+    const [formValue, setFormValue] = useState('Perth');
 
 
     function handleChange(event) {
-        setLocation(event.target.value)
-        console.log(location);
+        setFormValue(event.target.value);
+        console.log(formValue);
     };
     
     function handleSubmit(event) {
         event.preventDefault();
-        setLocation(event.target.value);
+        changeLocationName(formValue);
+        //event.target.value ? setFormValue(event.target.value) : setFormValue('');
     };
-
 
     return (
         <form onSubmit={handleSubmit}>
             <label>
             Location:
-            <input type="text" value={location} onChange={handleChange}/>
+            <input type="text" value={formValue} onChange={handleChange}/>
             </label>
             <input type="submit" value="Submit" />
           
