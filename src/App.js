8cdd94afe826d31b34de415386e5c4a4f5025e16
Julian Khan <juliankhan.b@gmail.com>
2022-10-24 15:07:57 +0800

    Add fetch to get example weather data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=' + 
+    'b1ab53cd687ca486f76e739d8fc1a32b')
+    .then(response => response.json())
+    .then(data => setWeatherData(data))
+  },[]);
+
+
   return (
     <div>
-      test
+      {`Current weather: ${weatherData.main.temp}`}
+    <form>
+      <button>test</button>
+    </form>
     </div>
   );
 }
