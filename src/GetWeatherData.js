f18f80d98d0e3e72f6d4c2251640c5440ea276ba
Julian Khan <juliankhan.b@gmail.com>
2022-10-26 07:21:26 +0800

    Add a refresh button to get updated weather data

--- src/GetWeatherData.js
+++ src/GetWeatherData.js
@@ -3,27 +3,30 @@ import React, {Component} from 'react';
 class GetWeatherData extends Component {
   constructor(props) {
     super(props);
-    this.state = {longitude: '', latitude: '', weatherData: ''};
-    //this.getWeatherData = this.getWeatherData.bind(this);
+
+    this.state = {weatherData: ''};
+
+    this.getWeatherData = this.getWeatherData.bind(this);
    }
 
-   componentDidMount() {
-    function getWeatherData() { 
+  getWeatherData() { 
       //fix this so that it responds to result of getlongandlat.js. Need to also secure API key with backend when implement it
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=-31.9558964&lon=115.8605801&appid=' + 
+
+    console.log(this.props.longitude, this.props.latitude);
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + this.props.latitude + '&lon=' + this.props.longitude + '&appid=' + 
     'b1ab53cd687ca486f76e739d8fc1a32b')
     .then(response => response.json())
-    .then(data => this.setState({weatherData: data}))
-  }
+    .then(data => this.setState({weatherData: data}));
+    }
 
-  getWeatherData();
-  console.log(weatherData);
-   
-   };
+   componentDidUpdate() {
+    //this.getWeatherData();
+   }
 
   render() {
-    console.log('hi');
-    return
+    //change so that this button is rendered by a UI component, not this fetch component (single responsibility principle)
+    return <button className='refresh-button' onClick={this.getWeatherData}>Refresh {console.log(this.state.weatherData)}</button>
   };
 
 };
