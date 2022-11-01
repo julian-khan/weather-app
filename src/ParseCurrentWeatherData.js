5170b12e176d1eb32ab2a339a72ab0865d9648c6
Julian Khan <juliankhan.b@gmail.com>
2022-11-02 06:48:01 +0800

    Create getCurrentDate function

--- src/ParseCurrentWeatherData.js
+++ src/ParseCurrentWeatherData.js
@@ -1,25 +1,19 @@
 import {useState, useEffect} from 'react';
 
 export default function ParseCurrentWeatherData({currentWeatherData}) {
+  let [dateAtTargetLocation, setDateAtTargetLocation] = useState(null);
 
-//const [summaries3H5D, setSummaries3H5D] = useState(null);
-console.log('currentweather...', currentWeatherData);
+  const getCurrentDate = () => {
+  let dateOriginalForm = new Date(currentWeatherData.dt * 1000);
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  console.log(dateOriginalForm.toLocaleDateString("en-US", options)); // Saturday, September 17, 2016
+  }
 
-const getCurrentDateAndTime = () => {
 
-  //finish this function's logic so that time automatically updates whenever a minute passes. Do not display seconds.
-
-let currentDate = new Date(currentWeatherData.dt * 1000);
-// Hours part from the timestamp
-let hours = currentDate.getHours();
-// Minutes part from the timestamp
-let minutes = "0" + currentDate.getMinutes();
-// Seconds part from the timestamp
-let seconds = "0" + currentDate.getSeconds();
-}
 
   useEffect(() => {
     if (currentWeatherData) {  
+      getCurrentDate();
     }
   });
 }
