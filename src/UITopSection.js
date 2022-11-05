7577ede3bc9f16157d3102f609c9a53cb27d9d52
Julian Khan <juliankhan.b@gmail.com>
2022-11-05 08:14:37 +0800

    Render min and max temperatures

--- src/UITopSection.js
+++ src/UITopSection.js
@@ -1,13 +1,20 @@
 import {useState, useEffect} from 'react';
 
 export default function UITopSection({processedCurrentWeatherData, todayMinAndMax}) {
-  
+
     return (
       <div>
         <div>{processedCurrentWeatherData.name}</div> // Add weather image depending on weather inline with city name
         <div>
-          {processedCurrentWeatherData.weather[0].main}
+          Today: {processedCurrentWeatherData.weather[0].main}
         </div>
+
+        <div>
+          <span>Min: {todayMinAndMax[0]}</span>
+          <span>Max: {todayMinAndMax[1]}</span>
+        </div>
+
+
 
 
       </div>
