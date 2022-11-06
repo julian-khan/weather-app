db5a8511ab139bd52dae202ffda7ae88423b9043
Julian Khan <juliankhan.b@gmail.com>
2022-11-06 11:08:25 +0800

    Add left and right margins to contact me section

--- src/ContactMeHeader.js
+++ src/ContactMeHeader.js
@@ -6,7 +6,7 @@ export default function ContactMeHeader() {
  
 
   return (
-    <div className="flex flex-row items-center justify-between">
+    <div className="flex flex-row items-center justify-between mx-20">
       <span>Contact me</span>
 
       <span className="flex flex-row items-center "> 
