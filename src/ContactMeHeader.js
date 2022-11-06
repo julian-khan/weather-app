3f0a4225d899f69015941132934c42dee9b80f81
Julian Khan <juliankhan.b@gmail.com>
2022-11-06 09:33:07 +0800

    Preparing to install react-icons

--- src/ContactMeHeader.js
+++ src/ContactMeHeader.js
@@ -4,7 +4,7 @@ export default function ContactMeHeader() {
 
   return (
     <div className="flex flex-row">
-      <span>Contact me</span><div>test 1</div><div>test 2</div>
+      <span>Contact me</span><div> <img src='./weather/ContactMeHeader-icons' /> </div><div>test 2</div>
     </div>
 
   );
