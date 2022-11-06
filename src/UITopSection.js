899880e4e8bf19c8d7f839f5a54870d2041fd1e8
Julian Khan <juliankhan.b@gmail.com>
2022-11-06 08:47:04 +0800

    Refactor component

--- src/UITopSection.js
+++ src/UITopSection.js
@@ -6,14 +6,10 @@ export default function UITopSection({processedCurrentWeatherData, todayMinAndMa
   https://commons.wikimedia.org/wiki/Category:Weather_icons 
   */
 
-  
-    return (
 
-      <div className='sm:text-center lg:text-left'>
-          <h1 class="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-            <span class="block xl:inline ">A weather app</span>
-          </h1>
-        <div>{processedCurrentWeatherData.name}</div> // Add weather image depending on weather inline with city name
+  /* Move the below JSX to separate UI component(s):
+
+  <div>{processedCurrentWeatherData.name}</div> // Add weather image depending on weather inline with city name
         <div>
           Today: {processedCurrentWeatherData.weather[0].main}
         </div>
@@ -23,5 +19,13 @@ export default function UITopSection({processedCurrentWeatherData, todayMinAndMa
           <span>Max: {todayMinAndMax[1]}</span>
         </div>
       </div>
+  */
+
+    return (
+      <div className='sm:text-center lg:text-left'>
+          <h1 class="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span class="block xl:inline ">A weather app</span>
+          </h1>
+      </div>      
     );
   }
