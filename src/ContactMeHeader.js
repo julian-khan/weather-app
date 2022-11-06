a9f91c9062720c8bbca04531d03f644c8dca9788
Julian Khan <juliankhan.b@gmail.com>
2022-11-06 11:06:40 +0800

    Add gap between text and icons

--- src/ContactMeHeader.js
+++ src/ContactMeHeader.js
@@ -6,10 +6,13 @@ export default function ContactMeHeader() {
  
 
   return (
-    <div className="flex flex-row items-center">
+    <div className="flex flex-row items-center justify-between">
       <span>Contact me</span>
-      <img src={linkedinicon} className='w-9' /> 
-      <img src={mail} className='w-10 align-middle'/> 
+
+      <span className="flex flex-row items-center "> 
+        <img src={linkedinicon} className='w-9' /> 
+        <img src={mail} className='w-10 align-middle'/> 
+      </span>
     </div >   
   );
 
