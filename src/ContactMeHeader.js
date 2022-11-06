9260ca21a7ba336fc60562917d32136e8d3b3995
Julian Khan <juliankhan.b@gmail.com>
2022-11-06 10:58:58 +0800

    Center flex items along vertical axis

--- src/ContactMeHeader.js
+++ src/ContactMeHeader.js
@@ -1,16 +1,16 @@
-import { ReactComponent as Mail } from './ContactMeHeader-icons/mail.svg';
+
+import mail from './ContactMeHeader-icons/mail.svg';
 import linkedinicon from './ContactMeHeader-icons/linkedinicon.png';
 
 export default function ContactMeHeader() {
  
 
   return (
-    <div className="flex flex-row">
-      <span>Contact me</span><div> <img src={linkedinicon} /> </div >   <Mail />
-    
-      
-    </div>
-
+    <div className="flex flex-row items-center">
+      <span>Contact me</span>
+      <img src={linkedinicon} className='h-10 w-10' /> 
+      <img src={mail} className='w-10 align-middle'/> 
+    </div >   
   );
 
     };
