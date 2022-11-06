d52b7fc511d7c23ee3411709721a01081720ff98
Julian Khan <juliankhan.b@gmail.com>
2022-11-06 10:59:28 +0800

    Resize LinkedIn icon

--- src/ContactMeHeader.js
+++ src/ContactMeHeader.js
@@ -8,7 +8,7 @@ export default function ContactMeHeader() {
   return (
     <div className="flex flex-row items-center">
       <span>Contact me</span>
-      <img src={linkedinicon} className='h-10 w-10' /> 
+      <img src={linkedinicon} className='w-9' /> 
       <img src={mail} className='w-10 align-middle'/> 
     </div >   
   );
