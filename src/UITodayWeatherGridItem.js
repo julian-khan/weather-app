66eba462b8ef3d6e4d04ff9e88b0c436c989979a
Julian Khan <juliankhan.b@gmail.com>
2022-11-10 06:48:38 +0800

    Make the today info grid a child component

---
+++ src/UITodayWeatherGridItem.js
@@ -0,0 +1,10 @@
+
+export default function UITodayWeatherGridItem (infoProp) { //make this component a generic css grid container
+  // that contains another component for each grid item, recursively rendered to contain all required information.
+
+  
+
+  
+};
+
+
