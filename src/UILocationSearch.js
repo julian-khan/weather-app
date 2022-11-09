bf2ea8a43d8e9b4a9cffa47a60fab0a7e796d156
Julian Khan <juliankhan.b@gmail.com>
2022-11-09 18:16:11 +0800

    Refactor UILocationSearch

--- src/UILocationSearch.js
+++ src/UILocationSearch.js
@@ -12,39 +12,17 @@ export default function LocationForm({setLocationName}) {
         setLocationName(formValue);
     };
 
-
-    /*
-
-   <form onSubmit={handleSubmit}>
-            <label for="citySearch" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">
-            Search for a city:  
-            </label>
-            <input id="citySearch" type="text" value={formValue} onChange={handleChange}/>
-            
-            <input type="submit" value="Submit" />
-        </form>
-
-
-    */
-
     return (
-    
+        <form onSubmit={handleSubmit} className='my-8 mx-5'>   
+            <label htmlFor="city-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+            <div className="relative">
+                <input type="text" id="city-search" value={formValue} onChange={handleChange} className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border
+                border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
+                dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
+                dark:focus:border-blue-500" placeholder="Search by entering the name of a city..." required="" />
 
-<form onSubmit={handleSubmit} className='my-8 mx-5'>   
-    <label htmlFor="city-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
-    <div className="relative">
-        <input type="text" id="city-search" value={formValue} onChange={handleChange} className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border
-         border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
-          dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-           dark:focus:border-blue-500" placeholder="Search by entering the name of a city..." required="" />
-
-        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-    </div>
-</form>
-
-
-
-
-     
+                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            </div>
+        </form>
         );
     };
