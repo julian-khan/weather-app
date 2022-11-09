80406106859237c5bb44cfab5b7a25b2fca9a579
Julian Khan <juliankhan.b@gmail.com>
2022-11-10 06:56:45 +0800

    Add padding and centering to CSS grid

--- src/UITodayWeatherGridItem.js
+++ src/UITodayWeatherGridItem.js
@@ -2,9 +2,19 @@
 export default function UITodayWeatherGridItem (infoProp) { //make this component a generic css grid container
   // that contains another component for each grid item, recursively rendered to contain all required information.
 
-  
 
-  
+
+  return (
+    <div className="className='flex flex-col items-center'">
+      <div>
+        property name
+      </div>
+      <div>
+        property value
+      </div>
+
+    </div>
+  ); 
 };
 
 
---
+++ src/UITodayWeatherInfoGrid.js
@@ -0,0 +1,19 @@
+import UITodayWeatherGridItem from "./UITodayWeatherGridItem";
+
+export default function UITodayWeatherInfoGrid ({processedCurrentWeatherData}) { //make this component a generic css grid container
+  // that contains another component for each grid item, recursively rendered to contain all required information.
+
+  return (
+    <div class="grid grid-cols-3 gap-4 place-items-center mx-8">
+  <UITodayWeatherGridItem />
+
+  <div>test 2</div>
+  <div>test 3 </div>
+  <div>test 4 </div>
+</div>
+  );
+
+  
+};
+
+
