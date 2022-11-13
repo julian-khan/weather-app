83d3c457eca112ce2237427e3b4cec12fe91a789
Julian Khan <juliankhan.b@gmail.com>
2022-11-13 09:19:46 +0800

    Create daily summary UI component

---
+++ src/DailySummary5D.js
@@ -0,0 +1,22 @@
+
+export default function DailySummary5D({dayName, dailySummary}) {
+
+  
+
+  return (
+    <div>
+      <div>
+        {dayName}
+      </div>
+      <div>
+       weathericon
+      </div>
+      <div>
+        min temp
+      </div>
+      <div>
+        max temp
+      </div>
+    </div>
+  );
+    };
