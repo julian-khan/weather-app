70095803a0036b3760b87c431805a7534a91b307
Julian Khan <juliankhan.b@gmail.com>
2022-11-13 10:51:37 +0800

    Add get weather icon logic to DailySummary5D.js

--- src/DailySummary5D.js
+++ src/DailySummary5D.js
@@ -1,12 +1,34 @@
+import ClearDay from './weather-icons/icons/ClearDay';
+import CloudsDay from './weather-icons/icons/CloudsDay';
+import ManyClouds from './weather-icons/icons/ManyClouds';
+import ShowersDay from './weather-icons/icons/ShowersDay';
+import Snow from './weather-icons/icons/Snow';
+import {ErrorNotFound} from './weather-icons/icons/ErrorNotFound';
 
-export default function DailySummary5D({dayName, dailySummary}) {
+export default function DailySummary5D({daySummary}) {
 
-  
+  const GetWeatherIcon = (daySummary) => {
+    const weatherDesc = daySummary.weather;
+    const visibility = daySummary.visibility;
+    const VisibilityCutoff = 8000; // This value for the visibility cutoff is arbitrary.
+
+    if (weatherDesc === 'Clear') {
+      return <ClearDay />
+    } else if (weatherDesc === 'Clouds' && visibility > VisibilityCutoff) {
+      return <CloudsDay />
+    } else if (weatherDesc === 'Clouds' && visibility < VisibilityCutoff) {
+      return <ManyClouds />
+    } else if (weatherDesc === 'Rain') {
+      return <ShowersDay />
+    } else if (weatherDesc === 'Snow') { 
+      return <Snow />
+    } else return <ErrorNotFound />
+  };
 
   return (
     <div>
       <div>
-        {dayName}
+        {daySummary.dayOfWeek}
       </div>
       <div>
        weathericon
--- src/UIFiveDayForecast.js
+++ src/UIFiveDayForecast.js
@@ -2,10 +2,19 @@ import DailySummary5D from "./DailySummary5D";
 
 export default function UIFiveDayForecast({threeHDDailySummaries}) {
 
-  
+  const testDayData = {
+    "dayNumber": 2,
+    "weather": "Clear",
+    "MinAndMaxTemp": [
+        14.18,
+        22.69
+    ],
+    "dayOfWeek": "Monday"
+  }
 
   return (
     <div>
+      <DailySummary5D daySummary={testDayData} />
  
     </div>
   );
