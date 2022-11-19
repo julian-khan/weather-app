5ccb6086bade9d077b40a73bb655b3acb11df76a
Julian Khan <juliankhan.b@gmail.com>
2022-11-19 09:58:26 +0800

    Add padding to current weather grid items

--- src/UITodayWeatherGridItem.js
+++ src/UITodayWeatherGridItem.js
@@ -2,7 +2,7 @@
 export default function UITodayWeatherGridItem ({propName, propValue}) { //make this component a flexbox container with the name above and value below
 
   return (
-    <div className="rounded-md flex flex-col justify-center items-center border-zinc-400 border-4 my-2 mx-2">
+    <div className="rounded-md flex flex-col justify-center items-center border-zinc-400 border-4 my-2 px-3">
       <div className="text-lg">
         {propName}
       </div>
