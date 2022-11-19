c8aba9edc8f6187a8e4a481899ecc7804a983792
Julian Khan <juliankhan.b@gmail.com>
2022-11-19 08:54:14 +0800

    Delete unused commented out code

--- src/UITopSection.js
+++ src/UITopSection.js
@@ -3,20 +3,6 @@ export default function UITopSection({processedCurrentWeatherData, todayMinAndMa
   https://commons.wikimedia.org/wiki/Category:Weather_icons 
   */
 
-
-  /* Move the below JSX to separate UI component(s):
-  <div>{processedCurrentWeatherData.name}</div> // Add weather image depending on weather inline with city name
-        <div>
-          Today: {processedCurrentWeatherData.weather[0].main}
-        </div>
-
-        <div>
-          <span>Min: {todayMinAndMax[0]}</span>
-          <span>Max: {todayMinAndMax[1]}</span>
-        </div>
-      </div>
-  */
-
     return (
       <div className='sm:text-center lg:text-left'>
           <h1 className="text-center text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
