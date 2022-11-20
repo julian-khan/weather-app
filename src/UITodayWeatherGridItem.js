c158c0528107cef7fc500aa9e3db27a3bb60967d
Julian Khan <juliankhan.b@gmail.com>
2022-11-20 12:15:06 +0800

    Centre text in current weather grid

--- src/UITodayWeatherGridItem.js
+++ src/UITodayWeatherGridItem.js
@@ -3,7 +3,7 @@ export default function UITodayWeatherGridItem ({propName, propValue}) { //make 
 
   return (
     <div className="rounded-md flex flex-col justify-center items-center border-zinc-400 border-4 px-3 h-24 w-40">
-      <div className="text-lg">
+      <div className="text-lg text-center">
         {propName}
       </div>
       <div>
