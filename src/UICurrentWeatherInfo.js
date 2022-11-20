2387799a98fde9cd75ee2a0cbb1f4ac1b144b736
Julian Khan <juliankhan.b@gmail.com>
2022-11-20 08:55:40 +0800

    Increase font size of current weather information

--- src/UICurrentWeatherInfo.js
+++ src/UICurrentWeatherInfo.js
@@ -10,13 +10,13 @@ export default function UICurrentWeatherInfo({processedCurrentWeatherData, units
     return (
     <div className='flex flex-col items-center my-8'>
       <div className='flex'> 
-      <span className='text-3xl px-3'>{processedCurrentWeatherData.name}</span>
+      <span className='text-xl sm:text-4xl md:text-5xl px-3'>{processedCurrentWeatherData.name}</span>
       <span> <GetWeatherIcon processedCurrentWeatherData={processedCurrentWeatherData} />
       </span>
     </div>
-    <div className='text-xl'>{capitaliseString(processedCurrentWeatherData.weather[0].description)}, {processedCurrentWeatherData.temp}{formatUnits(units)}</div>
+    <div className='text-xl sm:text-2xl md:text-3xl'>{capitaliseString(processedCurrentWeatherData.weather[0].description)}, {processedCurrentWeatherData.temp}{formatUnits(units)}</div>
 
-  <div className='text-sm'>
+  <div className='text-md sm:text-xl md:text-2xl'>
     <span>Min: {todayMinAndMax[0]} {formatUnits(units)}</span> <span>Max: {todayMinAndMax[1]} {formatUnits(units)} </span>
   </div>
   </div>
