f4b6c2da82d62103ff2e90f7fd6fda3a8c9c165c
Julian Khan <juliankhan.b@gmail.com>
2022-11-20 08:52:40 +0800

    Decrease font size for contact me section

--- src/UIContactMeHeader.js
+++ src/UIContactMeHeader.js
@@ -7,7 +7,7 @@ export default function UIContactMeHeader() {
 
   return (
     <div className="flex flex-row items-center justify-between px-20 ">
-      <span className = 'text-2xl tracking-tight text-gray-900 sm:text-3xl md:text-4xl'>Contact me</span>
+      <span className = 'text-xl sm:text-2xl md:text-3xl tracking-tight text-gray-900 '>Contact me</span>
 
       <span className="flex flex-row items-center "> 
         <img src={linkedinicon} className='w-9' /> 
