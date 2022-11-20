562fa69aee1375d7f6a80bdf35ab6bf7f1a3b349
Julian Khan <juliankhan.b@gmail.com>
2022-11-20 12:36:48 +0800

    Add padding and background colour to Contact Me

--- src/UIContactMeHeader.js
+++ src/UIContactMeHeader.js
@@ -6,7 +6,7 @@ export default function UIContactMeHeader() {
  
 
   return (
-    <div className="flex flex-row items-center justify-between px-20 ">
+    <div className="flex flex-row items-center justify-between py-2 px-20 bg-slate-200">
       <span className = 'text-xl sm:text-2xl md:text-3xl tracking-tight text-gray-900 '>Contact me</span>
       <span className="flex flex-row items-center "> 
       <a href="https://au.linkedin.com/in/julian-khan">
