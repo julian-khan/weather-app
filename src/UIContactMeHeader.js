851d8a3fd7cc783257e400436c4a1e3d5c41215f
Julian Khan <juliankhan.b@gmail.com>
2022-11-20 12:31:48 +0800

    Add link to my LinkedIn profile

--- src/UIContactMeHeader.js
+++ src/UIContactMeHeader.js
@@ -10,7 +10,9 @@ export default function UIContactMeHeader() {
       <span className = 'text-xl sm:text-2xl md:text-3xl tracking-tight text-gray-900 '>Contact me</span>
 
       <span className="flex flex-row items-center "> 
+      <a href="https://au.linkedin.com/in/julian-khan">
         <img src={linkedinicon} className='w-9' /> 
+        </a>
         <img src={mail} className='w-10 align-middle'/> 
       </span>
     </div >   
