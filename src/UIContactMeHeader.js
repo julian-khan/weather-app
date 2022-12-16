8bf118395966d31d44dda3d4b6479e089c13f40f
Julian Khan <juliankhan.b@gmail.com>
2022-12-16 09:54:59 +0800

    Remove mail icon and adjust header padding

--- src/UIContactMeHeader.js
+++ src/UIContactMeHeader.js
@@ -6,13 +6,12 @@ export default function UIContactMeHeader() {
  
 
   return (
-    <div className="flex flex-row items-center justify-between py-2 px-20 bg-slate-200">
+    <div className="flex flex-row items-center justify-between py-2 px-5 bg-slate-200">
       <span className = 'text-xl sm:text-2xl md:text-3xl tracking-tight text-gray-900 '>Contact me</span>
       <span className="flex flex-row items-center "> 
       <a href="https://au.linkedin.com/in/julian-khan">
         <img src={linkedinicon} className='w-9' /> 
       </a>
-        <img src={mail} className='w-10 align-middle'/> 
       </span>
     </div >   
   );
