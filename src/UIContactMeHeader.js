a7c551d7f333514fa6c60d0128c649e3313ac26e
Julian Khan <juliankhan.b@gmail.com>
2022-12-16 10:47:47 +0800

    Add transitions to header buttons

--- src/UIContactMeHeader.js
+++ src/UIContactMeHeader.js
@@ -10,12 +10,12 @@ export default function UIContactMeHeader() {
     <nav className=" flex items-center justify-between py-2 px-4 bg-slate-200">
       <div className = 'text-xl sm:text-2xl md:text-3xl tracking-tight text-gray-900 '>Contact me</div>
       <ul className="flex items-center">
-        <li className=" "> 
+        <li className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-320"> 
           <a href="https://au.linkedin.com/in/julian-khan">
             <img src={linkedinicon} className='w-9' /> 
           </a>
         </li>
-        <li> 
+        <li className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-320"> 
           <SettingsIcon />
         </li>
       </ul>
