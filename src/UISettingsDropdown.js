7996bbe6a1fd31b4c10fc50d285bf20172952082
Julian Khan <juliankhan.b@gmail.com>
2022-12-18 11:08:55 +0800

    Refactor useOutsideClick hook

--- src/UISettingsDropdown.js
+++ src/UISettingsDropdown.js
@@ -1,16 +1,8 @@
 import {useRef, useEffect, useState } from "react";
 import SettingsIcon from "./weather-icons/icons/SettingsIcon";
-import use3H5DForecast from "./use3H5DForecast";
-
 
 export default function UISettingsDropdown({viewMode, setViewMode, units, setUnits}) {
   const [opened, setOpened] = useState(false);
-
-  function GenerateSettingsIcon() { 
-    return (
-      <SettingsIcon />
-    );
-  }
 
   function DropDownSection(props) {
     const itemStyling = "border-solid border-zinc-400 border-2 rounded-md transition ease-in-out delay-100 hover:bg-sky-500 hover:ring-sky-500 duration-320 dark:text-slate-400 text-xl"
@@ -26,8 +18,8 @@ export default function UISettingsDropdown({viewMode, setViewMode, units, setUni
   function useOutsideClick(ref) {
     useEffect(() => {
       function handleClickOutside(event) {
-        if (ref.current && !ref.current.contains(event.target) && opened) {
-          opened === true? setOpened(false): setOpened(true);
+        if (ref.current && !ref.current.contains(event.target) && opened ===true) {
+          setOpened(false);
         }
       }
       document.addEventListener("mousedown", handleClickOutside);
@@ -38,12 +30,12 @@ export default function UISettingsDropdown({viewMode, setViewMode, units, setUni
     }, [ref, opened]);
   }
 
-    const wrapperRef = useRef(null);
-    useOutsideClick(wrapperRef);
+  const wrapperRef = useRef(null);
+  useOutsideClick(wrapperRef);
 
   return (
     <li ref={wrapperRef} onClick={() => {setTimeout(()=> {setOpened(!opened)}, 150)}} className="transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-320"> 
-    <GenerateSettingsIcon />
+    <SettingsIcon />
     {opened && <DropDownSection/>}
     </li>
     );
