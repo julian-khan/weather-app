e0cea83f41ea4038949ed7b31b477ffcce87f2d4
Julian Khan <juliankhan.b@gmail.com>
2022-12-20 15:54:42 +0800

    Refactor App.js to improve code readability

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import {useState, useEffect} from 'react';
 
-//Data fetching and processing components
-import use3H5DForecast from './use3H5DForecast';
-import Parse3H5D from './Parse3H5D';
+//Data fetching and processing custom hooks
 import useCurrentWeatherData from './useCurrentWeatherData';
+import useParse3H5D from './useParse3H5D';
+import use3H5DForecast from './use3H5DForecast';
 
 //UI components
 import UIAppContainer from './UIAppContainer';
@@ -18,13 +18,10 @@ function App() {
   const [latitude, setLatitude] = useState(-31.953512);
 
   const [currentWeatherData, setCurrentWeatherData] = useState(null);
-
   const [processedCurrentWeatherData, setProcessedCurrentWeatherData] = useState(null);
-
 
   const threeH5DData = use3H5DForecast(longitude, latitude, units);
   const [threeHDDailySummaries, setThreeHDDailySummaries] = useState(null);
-
 
   function useLongAndLat() {
     useEffect(() => {
@@ -39,27 +36,23 @@ function App() {
     return 
     };
 
-    function useRetrieveCurrentWeatherData() {
-      useEffect(() => { 
-        //Need to secure API key with backend when implement it
-        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + 
-        'b1ab53cd687ca486f76e739d8fc1a32b' + '&units=' + units)
-        .then(response => response.json())
-        .then(data => {setCurrentWeatherData(data)});
-        }, [latitude, longitude, units])
-      }
+  function useRetrieveCurrentWeatherData() {
+    useEffect(() => { 
+      //Need to secure API key with backend when implement it
+      fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + 
+      'b1ab53cd687ca486f76e739d8fc1a32b' + '&units=' + units)
+      .then(response => response.json())
+      .then(data => {setCurrentWeatherData(data)});
+      }, [latitude, longitude, units])
+    }
 
     useLongAndLat()
     useRetrieveCurrentWeatherData()
     useCurrentWeatherData(currentWeatherData, setProcessedCurrentWeatherData)
+    useParse3H5D(processedCurrentWeatherData, threeH5DData, setThreeHDDailySummaries)
 
- //add this prop to parse3H5D: 
- // currentTemp={processedCurrentWeatherData.temp} 
- 
   return (
-    <div className={viewMode}>
-      <Parse3H5D threeH5DData={threeH5DData} setThreeHDDailySummaries={setThreeHDDailySummaries} /> 
-
+    <div className={viewMode}> 
       <UIAppContainer 
       processedCurrentWeatherData={processedCurrentWeatherData} 
       threeHDDailySummaries={threeHDDailySummaries}
@@ -69,9 +62,7 @@ function App() {
       viewMode={viewMode}
       setViewMode = {setViewMode}
       />
-
     </div>
   );
 };
-
 export default App;
---
+++ src/useParse3H5D.js
@@ -0,0 +1,49 @@
+import {useState, useEffect} from 'react';
+import * as dataHandling from './functions/dataHandling3H5DModule'
+
+export default function useParse3H5D(processedCurrentWeatherData, threeH5DData, setThreeHDDailySummaries) {
+//will take the original full 3h5d json object as a prop
+
+  function getSummaries3H5D(separated3H5D) {
+    const currentTemp = processedCurrentWeatherData.temp
+    let summarisedDay3H = [];
+
+    for (let i = 0; i <= separated3H5D.length - 1; i++) {
+      let dayListOutput = {};
+      dayListOutput.dayNumber = i+1;
+
+      const dayForData = separated3H5D[i];
+
+      const weatherDescCount = dataHandling.getDailyWeatherDescriptions(dayForData);
+      const mostCommonWeather = dataHandling.getMostCommonWeatherDescription(weatherDescCount);
+      dayListOutput.weather = mostCommonWeather;
+
+      const dailyTempArray = dataHandling.getDailyTempArray(dayForData);
+      let MinAndMaxTemp = dataHandling.getMinAndMaxTemp(dailyTempArray);
+      if (i===0 && currentTemp < MinAndMaxTemp[0]) {
+        MinAndMaxTemp[0] = currentTemp
+      } else if (i===0 && currentTemp > MinAndMaxTemp[1]) {
+        MinAndMaxTemp[1] = currentTemp
+      }
+      dayListOutput.MinAndMaxTemp = MinAndMaxTemp;
+
+      const date = new Date(dayForData[0].dt * 1000);
+      const dayOfWeek = new Intl.DateTimeFormat('en-GB', { weekday: 'long'}).format(date);
+      dayListOutput.dayOfWeek = dayOfWeek;
+
+      const averageVisibility = dataHandling.calculateAvVisibility(dayForData);
+      dayListOutput.averageVisibility = averageVisibility;
+
+      summarisedDay3H.push(dayListOutput);
+  }
+  return summarisedDay3H;
+  }
+
+  useEffect(() => {
+    if (processedCurrentWeatherData && threeH5DData) {  
+      const separated3H5D = dataHandling.separate3H5DDataToDays(threeH5DData);
+      const summaryOutput = getSummaries3H5D(separated3H5D);
+      setThreeHDDailySummaries(summaryOutput);
+    }
+  }, [processedCurrentWeatherData, threeH5DData]);
+}
