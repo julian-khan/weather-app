5c0ff77f2f37605593d505790235895a71b91bf1
Julian Khan <juliankhan.b@gmail.com>
2022-12-21 12:29:54 +0800

    Italicise input and other styling

--- src/UILocationSearch.js
+++ src/UILocationSearch.js
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 
 export default function LocationForm({setLocationName}) {
-    const [formValue, setFormValue] = useState('Perth');
+    const [formValue, setFormValue] = useState('');
 
     function handleChange(event) {
         setFormValue(event.target.value);
@@ -12,16 +12,19 @@ export default function LocationForm({setLocationName}) {
         setLocationName(formValue);
     };
 
+    let inputCustomisation = 'italic'
+
     return (
         <form onSubmit={handleSubmit} className='mt-6 mb-10 mx-auto w-2/4 max-w-[800px] min-w-[450px]'>   
             <label htmlFor="city-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
             <div className="relative">
-                <input type="text" id="city-search" value={formValue} onChange={handleChange} className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border
-                border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
-                dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
-                dark:focus:border-blue-500" placeholder="Search by entering the name of a city..." required="" />
-
-                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                <input type="text" id="city-search" value={formValue} onChange={handleChange} className={inputCustomisation} className='block p-4
+                pl-12 w-full italic text-sm text-gray-600 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500
+                focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
+                dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder="Search by entering the name of a city..." required="" />
+                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 focus:ring-4 focus:outline-none 
+                font-medium rounded-lg text-sm px-4 py-2 bg-sky-500 hover:bg-blue-800 focus:ring-blue-300
+                dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">Search</button>
             </div>
         </form>
         );
