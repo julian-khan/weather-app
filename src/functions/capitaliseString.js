5f7a9c8761752e24603e30c2d0b989b3183de956
Julian Khan <juliankhan.b@gmail.com>
2022-12-22 14:53:27 +0800

    Refactor capitaliseString function

--- src/functions/capitaliseString.js
+++ src/functions/capitaliseString.js
@@ -1,4 +1,3 @@
 export default function capitaliseString(str) {
-    const returnStr = str.charAt(0).toUpperCase() + str.slice(1);
-    return returnStr;
-  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
