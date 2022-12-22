c51ce5c1343d389b019ba653419257d4935bb179
Julian Khan <juliankhan.b@gmail.com>
2022-12-22 11:38:58 +0800

    Add link to project README file in header

--- src/UIContactMeHeader.js
+++ src/UIContactMeHeader.js
@@ -1,15 +1,12 @@
-
-import mail from './ContactMeHeader-icons/mail.svg';
-import SettingsIcon from './weather-icons/icons/SettingsIcon';
 import linkedinicon from './ContactMeHeader-icons/linkedinicon.png';
 import UISettingsDropdown from './UISettingsDropdown';
 
 export default function UIContactMeHeader({viewMode, setViewMode, units, setUnits}) {
- 
-
   return (
     <nav className=" flex items-center justify-between py-2 px-4 bg-slate-200">
-      <div className = 'text-xl sm:text-2xl md:text-3xl tracking-tight text-gray-900 '>Contact me</div>
+      <a href="https://github.com/julian-khan/weather-app#readme">
+        <div className = 'text-sm tracking-tight text-gray-900 '>Please view the README file on GitHub</div>
+      </a>
       <ul className="flex items-center">
         <li className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-320"> 
           <a href="https://au.linkedin.com/in/julian-khan">
