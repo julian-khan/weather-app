15603ec583765d483a220c5fb5af2031dfc3d95e
Julian Khan <juliankhan.b@gmail.com>
2022-12-22 12:29:16 +0800

    Fix current time formatting for 12-12:59 pm

--- src/functions/getTimeAtTargetLocation.js
+++ src/functions/getTimeAtTargetLocation.js
@@ -1,4 +1,9 @@
 export default function getTimeAtTargetLocation(dateObj) {
     const options = {hour12: 'true', hour: 'numeric', minute: 'numeric',  timeZone: 'UTC'}
-    return dateObj.toLocaleString('en-GB', options)    
+    let time_str = dateObj.toLocaleString('en-GB', options)
+    
+    if (time_str[0] === '0') {
+      time_str = '12' + time_str.slice(1)
+    }
+    return time_str
   }
