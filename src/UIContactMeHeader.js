ec4d8b7f019fffa7d48c1344468acaf08efa0b71
Julian Khan <juliankhan.b@gmail.com>
2022-12-22 11:42:44 +0800

    Update hover effects in header

--- src/UIContactMeHeader.js
+++ src/UIContactMeHeader.js
@@ -4,11 +4,12 @@ import UISettingsDropdown from './UISettingsDropdown';
 export default function UIContactMeHeader({viewMode, setViewMode, units, setUnits}) {
   return (
     <nav className=" flex items-center justify-between py-2 px-4 bg-slate-200">
-      <a href="https://github.com/julian-khan/weather-app#readme">
+      <a className="transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-105 duration-320"
+      href="https://github.com/julian-khan/weather-app#readme">
         <div className = 'text-sm tracking-tight text-gray-900 '>Please view the README file on GitHub</div>
       </a>
       <ul className="flex items-center">
-        <li className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-320"> 
+        <li className="transition ease-in-out delay-150 hover:-translate-y-0.5 hover:scale-105 duration-320"> 
           <a href="https://au.linkedin.com/in/julian-khan">
             <img src={linkedinicon} className='w-9' /> 
           </a>
