12545da290bfbca782419a7ff7ce7148a3d2d711
Julian Khan <juliankhan.b@gmail.com>
2022-12-22 12:11:35 +0800

    Delete unused template files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// I intend to add testing to this weather application.
+
+/*
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+*/
