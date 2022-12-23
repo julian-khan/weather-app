5f01338066e888f9eae095027d5dddafc54bcd12
Julian Khan <juliankhan.b@gmail.com>
2022-12-23 14:36:13 +0800

    Add fog icon

--- src/GetWeatherIcon.js
+++ src/GetWeatherIcon.js
@@ -6,6 +6,7 @@ import ManyClouds from "./weather-icons/icons/ManyClouds";
 import ShowersDay from "./weather-icons/icons/ShowersDay";
 import ShowersNight from "./weather-icons/icons/ShowersNight";
 import Snow from "./weather-icons/icons/Snow";
+import Fog from "./weather-icons/icons/Fog";
 
 
 export default function GetWeatherIcon({ processedCurrentWeatherData }) {
@@ -21,7 +22,7 @@ export default function GetWeatherIcon({ processedCurrentWeatherData }) {
   const isDay = (currentDt, sunsetDt) => {
     //Takes two Unix timestamps as arguments
     return currentDt < sunsetDt ? true : false;
-  };
+  }
 
   if (isDay(currentDateTime, todaySunsetDateTime) && weatherDesc === "Clear") {
     return <ClearDay />;
@@ -62,5 +63,10 @@ export default function GetWeatherIcon({ processedCurrentWeatherData }) {
     weatherDesc === "Drizzle"
   ) {
     return <ShowersNight />;
-  } else return <div />;
+  } else if (
+    weatherDesc === "Fog" 
+  ) {
+    return <Fog />;
+  }
+  else return <div />;
 }
---
+++ src/weather-icons/icons/Fog.js
@@ -0,0 +1,77 @@
+import React from "react";
+
+function Fog() {
+    /* Fog icon attribution:
+    https://freeicons.io/index.php/profile/730 */   
+    return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="64"
+      height="64"
+      x="0"
+      y="0"
+      version="1.1"
+      viewBox="0 0 64 64"
+      xmlSpace="preserve"
+    >
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M56 45c4.19 0 7-2.81 7-7 0-4.189-2.81-9-7-9 0-10.475-9.525-18-20-18-9.271 0-17.348 6.211-19 15h-2c-5.238 0-9 4.762-9 10s3.762 9 9 9"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M29.999 11.752A12.945 12.945 0 0022 9C14.82 9 9 14.82 9 22c0 2.577.707 3.979 2 6"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M6 22L0 22"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M22 6L22 0"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M10 10L4 4"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M17 45L54 45"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M17 51L54 51"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeMiterlimit="10"
+        strokeWidth="2"
+        d="M17 57L54 57"
+      ></path>
+    </svg>
+  );
+}
+
+export default Fog;
